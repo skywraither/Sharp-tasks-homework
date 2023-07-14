c05caf4abc28bba14fefd3f6fb15378e87fdfb11
Alex <tut@ya.ru>
2023-07-14 08:48:23 +0300

    добавил задания 6 и 7

---
+++ homework 6/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+        Console.Write("Введите количество чисел: ");
+        int m = int.Parse(Console.ReadLine()!);
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine()!);
+
+            if (number > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество чисел, больших нуля: " + count);
+
+
